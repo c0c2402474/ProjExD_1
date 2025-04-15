b0b35a6fbd276c207b41910805669351270e1bb2
c0c2402474 <c0c2402474@edu.teu.ac.jp>
2025-04-15 17:36:29 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -15,29 +15,45 @@ def main():
     kk3_img = pg.transform.flip(kk3_img, True, False)#こうかとん反転2
     kk3_rct = kk3_img.get_rect()#rect抽出１０
     kk3_rct.center = 300,200#初期座標１０
-    
+    tate = 0
+    yoko = 0
     tmr = 0
     
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
         key_lst = pg.key.get_pressed()#キー検知１０－３
         if key_lst[pg.K_UP]:#こうかとん移動10-4
-            kk3_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
-            kk3_rct.move_ip((0, 1))
-        if key_lst[pg.K_RIGHT]:
-            kk3_rct.move_ip((2, 0))
-        if key_lst[pg.K_LEFT]:
-            kk3_rct.move_ip((-2, 0))
-        else:
-            kk3_rct.move_ip((-1, 0))
+            yoko = -1
+            tate = -1
+
+            # kk3_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            yoko = -1
+            tate = 1
+            # kk3_rct.move_ip((0, 1))
+        elif key_lst[pg.K_RIGHT]:
+            yoko = 2
+            tate = 0
+            # kk3_rct.move_ip((2, 0))
+        elif key_lst[pg.K_LEFT]:
+            yoko = -2
+            tate = 0
+            # kk3_rct.move_ip((-2, 0))
+        else:#押してないとき左に動く
+            yoko = -1
+            tate = 0
+            # kk3_rct.move_ip((-1, 0))
+        kk3_rct.move_ip((yoko,tate))
         x = tmr % 3200#ループ９
+        # y = 200
         screen.blit(bg_img, [-x, 0])#背景移動6
         screen.blit(bg2_img, [-x+1600, 0])#ｂｇ二枚目7
-        screen.blit(kk3_img, kk3_rct)#こうかとん反映4
         screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(kk3_img, kk3_rct)#こうかとん反映4
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)#FPS変更５
